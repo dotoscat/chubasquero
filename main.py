9ea295c282ffe9ccb9cae1eb4ae4baf82f9edf69
Oscar Triano dotoscat <user@example.com>
2017-12-23 18:42:03 +0100

    Open a webbrowser to the app

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import webbrowser
 from wsgiref.simple_server import make_server
 from flask import Flask
+
+HOST = "127.0.0.1"
+PORT = 8080
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "Hello world"
 
-with make_server("127.0.0.1", 8080, app) as httpd:
+with make_server(HOST, PORT, app) as httpd:
     print(httpd.server_address)
+    webbrowser.open("http://{}:{}".format(HOST, PORT))
     httpd.serve_forever()
