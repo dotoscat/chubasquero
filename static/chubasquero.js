5abca40c6437acbab5485766c69dd88b85ea878c
Oscar Triano dotoscat <user@example.com>
2017-12-23 23:13:52 +0100

    Render posts

--- static/chubasquero.js
+++ static/chubasquero.js
@@ -10,16 +10,34 @@ function previewSite () {
     console.log("previsualizar sitio");
 }
 
+function getPostList () {
+    return new Promise((success, fail) => {
+        setTimeout(() => {
+            success([
+                "Uno",
+                "Dos",
+                "Tres",
+            ]);
+        }, 2000);
+    });
+}
+
 const contenido = document.getElementById("contenido");
 
 const chubasquero = new Vue({
+    delimiters: ["${", "}"],
     el: '#chubasquero',
     data: {
-        posts: []
+        posts: ["Uno", "Dos"]
     },
     methods: {
         managePosts: managePosts,
         generateSite: generateSite,
         previewSite: previewSite,
+    },
+    watch: {
+        posts: function (value) {
+            this.posts = value;
+        }
     }
 });
