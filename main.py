42a088553fc3a943b5eca7e58cd0b80c480765d5
Oscar Triano dotoscat <user@example.com>
2017-12-23 18:22:16 +0100

    Start simple server at localhost

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from wsgiref.simple_server import make_server
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "Hello world"
+
+with make_server("127.0.0.1", 8080, app) as httpd:
+    print(httpd.server_address)
+    httpd.serve_forever()
