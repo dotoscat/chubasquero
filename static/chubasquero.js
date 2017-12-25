4c9b7e97f10642ee986373c519c4ba25334f0c6f
Oscar Triano dotoscat <user@example.com>
2017-12-25 12:35:45 +0100

    Use 2 spaces for nesting

--- static/chubasquero.js
+++ static/chubasquero.js
@@ -1,22 +1,22 @@
 'use strict';
 
 function createPost(){
-    return {
-        meta: {
-            date: '',
-            modified: '',
-            tags: '',
-            category: '',
-            slug: '',
-            authors: '',
-            summary: '',
-            lang: '',
-            translation: ''
-        },
-        content: {
-            en: ""
-        },
-    };
+  return {
+    meta: {
+      date: '',
+      modified: '',
+      tags: '',
+      category: '',
+      slug: '',
+      authors: '',
+      summary: '',
+      lang: '',
+      translation: ''
+    },
+    content: {
+      en: ""
+    },
+  };
 }
 
 /**
@@ -28,81 +28,81 @@ function createPost(){
  * @returns {promise} A json promise 
  */
 function requestGetToServer(route) {
-    const request = fetch(CHUBASQUERO_SERVER + route);
-    return request.then((response) => response.json());
+  const request = fetch(CHUBASQUERO_SERVER + route);
+  return request.then((response) => response.json());
 }
 
 function previewSite () {
-    console.log("previsualizar sitio");
+  console.log("previsualizar sitio");
 }
 
 const chubasquero = new Vue({
-    delimiters: ["${", "}"],
-    el: '#chubasquero',
-    data: {
-        post: null,
-        posts: [],
-        loadingPosts: false,
-        showPosts: false,
-        showEditor: false,
-        showGenerateSite: false,
-        generatingSite: false,
-        serverResponse: {returncode: 0, stdout: "", stderr: ""}
+  delimiters: ["${", "}"],
+  el: '#chubasquero',
+  data: {
+    post: null,
+    posts: [],
+    loadingPosts: false,
+    showPosts: false,
+    showEditor: false,
+    showGenerateSite: false,
+    generatingSite: false,
+    serverResponse: {returncode: 0, stdout: "", stderr: ""}
+  },
+  methods: {
+    /**
+     * Clean the current sections.
+     * 
+     * Actually what this does is set all the show group to false
+     */
+    cleanView: function (){
+      this.showPosts = false,
+      this.showEditor = false,
+      this.showGenerateSite = false
     },
-    methods: {
-        /**
-         * Clean the current sections.
-         * 
-         * Actually what this does is set all the show group to false
-         */
-        cleanView: function (){
-            this.showPosts = false,
-            this.showEditor = false,
-            this.showGenerateSite = false
-        },
-        /**
-         * Prepare content to edit a post
-         */
-        newPost: function () {
-            this.cleanView();
-            this.showEditor = true;
-            this.post = createPost();
-            console.log("new post")
-        },
-        /**
-         * Change content to manage posts
-         */
-        managePosts: function () {
-            this.cleanView();
-            this.showPosts = true;
-            this.loadingPosts = true;
-            this.posts = [];
-            requestGetToServer("/posts")
-            .then((posts) => {
-                console.log("posts", posts);
-                this.$set(this, "posts", posts);
-                this.loadingPosts = false;
-            }, (error) => {
-                console.error("posts", error);
-                this.loadingPosts = false;
-            });
-        },
-        /**
-         * Generate a site and wait for server response
-         */
-        generateSite: function () {
-            this.cleanView();
-            this.showGenerateSite = true;
-            this.generatingSite = true;
-            requestGetToServer("/generate-site")
-            .then((response) => {
-                console.log("response", response);
-                this.$set(this, "serverResponse", response);
-                this.generatingSite = false;
-            }, (error) => {
-                this.generatingSite = false;
-            });
-        },
-        previewSite: previewSite,
-    }
+    /**
+     * Prepare content to edit a post
+     */
+    newPost: function () {
+      this.cleanView();
+      this.showEditor = true;
+      this.post = createPost();
+      console.log("new post")
+    },
+    /**
+     * Change content to manage posts
+     */
+    managePosts: function () {
+      this.cleanView();
+      this.showPosts = true;
+      this.loadingPosts = true;
+      this.posts = [];
+      requestGetToServer("/posts")
+      .then((posts) => {
+        console.log("posts", posts);
+        this.$set(this, "posts", posts);
+        this.loadingPosts = false;
+      }, (error) => {
+        console.error("posts", error);
+        this.loadingPosts = false;
+      });
+    },
+    /**
+     * Generate a site and wait for server response
+     */
+    generateSite: function () {
+      this.cleanView();
+      this.showGenerateSite = true;
+      this.generatingSite = true;
+      requestGetToServer("/generate-site")
+      .then((response) => {
+        console.log("response", response);
+        this.$set(this, "serverResponse", response);
+        this.generatingSite = false;
+      }, (error) => {
+        this.generatingSite = false;
+      });
+    },
+    previewSite: previewSite,
+  }
 });
