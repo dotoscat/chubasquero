3379681cfad4112754e3a8262599688fbe80261d
Oscar Triano dotoscat <user@example.com>
2017-12-30 20:30:24 +0100

    Show up view to edit post

--- static/chubasquero.js
+++ static/chubasquero.js
@@ -13,6 +13,30 @@ function requestGetToServer(route) {
   return request.then((response) => response.json());
 }
 
+/**
+ * Makes a POST request to server with Fetch API.
+ * 
+ * The data passed is converted to JSON and sent to the server.
+ * 
+ * This functions depends of CHUBASQUERO_SERVER.
+ * 
+ * @param {string} route Route endpoint
+ * @param {object} data Data with method POST (don't confuse with the post)
+ * @returns {promise} A fetch promise
+ */
+function requestPostToServer(route, data) {
+  const endpoint = CHUBASQUERO_SERVER + route;
+  const sendObject = JSON.stringify(data);
+  const fetchInit = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: sendObject
+  };
+  return fetch(endpoint, fetchInit);
+}
+
 function previewSite () {
   console.log("previsualizar sitio");
 }
@@ -22,6 +46,7 @@ const chubasquero = new Vue({
   el: '#chubasquero',
   data: {
     post: new Post(),
+    isNewPost: false,
     posts: [],
     loadingPosts: false,
     showPosts: false,
@@ -38,6 +63,15 @@ const chubasquero = new Vue({
     this.savePostNotification = document.getElementById("savepost-notification");
   },
   methods: {
+    editPost: function (post) {
+      this.isNewPost = false;
+      this.cleanView();
+      this.showEditor = true;
+      // TODO: ask the server for the content based on post.slug and create a Post with it
+      this.$set(this, "post", new Post());
+      this.startAutosave();
+      console.log("Edit post", post);
+    },
     /**
      * Sends the content of the post as json to the server
      */
@@ -58,7 +92,6 @@ const chubasquero = new Vue({
         },
         body: sendObject
       };
-      const sendPostNotification = document.getElementB
       fetch(endpoint, fetchInit).then((response) => {
           // TODO: Display on screen discretly the result of the operation
           const message = '"' + this.post.title + '" is saved.';
@@ -104,6 +137,7 @@ const chubasquero = new Vue({
      * Prepare content to edit a post
      */
     newPost: function () {
+      this.isNewPost = true;
       this.cleanView();
       this.showEditor = true;
       this.$set(this, "post", new Post());
