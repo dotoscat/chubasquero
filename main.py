4d7f0ec6ed553d6a54af2c7ebc4774d5628c3b8b
Oscar Triano dotoscat <user@example.com>
2017-12-31 12:04:35 +0100

    Move functions related with posts to module

--- main.py
+++ main.py
@@ -4,107 +4,13 @@ import json
 import webbrowser
 from wsgiref.simple_server import make_server
 import flask
-from docutils.core import publish_doctree
-from docutils import nodes
-
-try:
-    import pelicanconf
-except ImportError:
-    print("pelicanconf not found.")
-    exit(1)
+import chubasquero
 
 HOST = "127.0.0.1"
 PORT = 8080
 URL = "http://{}:{}".format(HOST, PORT)
-CONTENT_PATH = os.path.abspath(pelicanconf.PATH)
 
 app = flask.Flask(__name__, static_url_path='')
-
-def get_metadata_from_file(post_file):
-    """Get the just the metadata from the post file."""
-    post_doctree = publish_doctree(post_file.read())
-    return get_metadata_from_doctree(post_doctree)
-
-def get_metadata_from_doctree(post_tree):
-    """Returns metadata post as a dict.
-    
-    Parameters:
-        post_tree (:obj:`docutils.nodes.document`): Post tree
-    
-    Returns:
-        (dict|None): meta info from the post. None if metadata is not found.
-    """
-    metadata = {}
-    docinfo = post_tree[1]
-    if not isinstance(docinfo, nodes.docinfo):
-        return None
-    for elem in docinfo:
-        if isinstance(elem, nodes.date):
-            metadata["date"] = elem.astext()
-        else:
-            key = elem[0].astext()
-            if key in ("authors", "tags", "category"):
-                value = [v.strip() for v in elem[1].astext().split(',')]
-            else:
-                value = elem[1].astext()
-            metadata[key] = value
-    return metadata
-
-def do_with_file(path, action):
-    """Do some *action* with an file opened with path.
-    
-    Parameters:
-        path (str): Path of the file
-        action (callable): A callable with a file descriptor as first argument.
-    
-    Returns:
-        The return value of *action*
-    """
-    with open(path) as this_file:
-        return action(this_file)
-
-def get_post_list():
-    """Returns a post list stored on :obj:`pelicanconf.PATH`."""
-    return [do_with_file(post.path, get_metadata_from_file)
-        for post in os.scandir(CONTENT_PATH)]
-
-def save_post_locally(post):
-    """Saves the post object to disk.
-    
-    Parameters:
-        post (dict): Post to save to disk.
-    """
-    title = post["_title"]
-    content = post["content"]
-    
-    post_body = ""
-    post_body += title + '\n'
-    post_body += '#'*len(title) + '\n\n'
-    for key in post["meta"]:
-        value = post["meta"][key]
-        if value is None: continue
-        if isinstance(value, str):
-            post_body += ":{}: {}\n".format(key, value)
-        elif isinstance(value, list):
-            if not value: continue
-            post_body += ":{}: ".format(key)
-            for i, element in enumerate(value):
-                if i == len(value):
-                    post_body += element
-                else:
-                    post_body += element + ", "
-            post_body += '\n'
-    post_body += '\n\n'
-    post_body += content + '\n'
-    
-    print("save post", post)
-    print("post body", post_body)
-    
-    filename = post["meta"]["slug"] + ".rst"
-    file_path = os.path.join(CONTENT_PATH, filename)
-    
-    with open(file_path, "w") as post_file:
-        post_file.write(post_body)
 
 @app.route("/")
 def index():
@@ -115,11 +21,11 @@ def index():
     pelicanconf defined from the user.
     """
     return flask.render_template("index.html"
-        , **{"server": URL, "defaultLang": pelicanconf.DEFAULT_LANG})
+        , **{"server": URL, "defaultLang": chubasquero.pelicanconf.DEFAULT_LANG})
 
 @app.route("/posts", methods=["GET"])
 def get_posts():
-    posts = json.dumps(get_post_list())
+    posts = json.dumps(chubasquero.get_post_list())
     return posts
 
 @app.route("/post", methods=["POST"])
@@ -135,7 +41,7 @@ def save_post():
 def get_post(slug):
     """Returns a JSON object of the post to be used by javascript Post class."""
     filename = slug + ".rst";
-    filepath = os.path.join(CONTENT_PATH, filename);
+    filepath = os.path.join(chubasquero.CONTENT_PATH, filename);
     # TODO: Returns post as Post class for the frontend
     
     return json.dumps({"serverResponse": "return {} asked from the client".format(filepath)});
