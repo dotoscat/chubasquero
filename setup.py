a101b10094d3bf89e1cac388d30d522e94d93732
Oscar Triano dotoscat <user@example.com>
2018-01-06 19:23:44 +0100

    Change setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Chubasquero",
-    version="0.9.0",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=["pelican>=3.7.1"],
     entry_points = {
@@ -11,4 +11,19 @@ setup(
     package_data = {
         "chubasquero": ["static/*", "templates/*"]
     },
+    author="Oscar (dotoscat) Triano",
+    description="A CMS built on top of Pelican",
+    url="https://github.com/dotoscat/chubasquero",
+    license="AGPL-3.0",
+    selectors=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Flask",
+        "Framework :: Pelican",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Desktop Environment",
+        "Topic :: Utilities"
+    ],
 )
